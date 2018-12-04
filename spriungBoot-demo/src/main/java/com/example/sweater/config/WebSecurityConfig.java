e33defd35596bfebb5f4ecea5a8d90aea4d3dde6
Dkhmil <0973138620dh@gmail.com>
2018-12-04 22:19:56 +0200

    security

---
+++ spriungBoot-demo/src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.sweater.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select USERNAME,PASSWORD,ACTIVE from USERS where USERNAME =?")
+                .authoritiesByUsernameQuery("select USERNAME,ROLES from USERS join USER_ROLE on USERS.ID = USER_ROLE.ID where USERS.USERNAME = ?");
+    }
+}
---
+++ spriungBoot-demo/src/main/java/com/example/sweater/controller/RegisterController.java
@@ -0,0 +1,39 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegisterController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String registration() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+        if (userFromDB != null) {
+            model.put("message", "User exist");
+            return "register";
+
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+        return "redirect:/login";
+
+    }
+}
